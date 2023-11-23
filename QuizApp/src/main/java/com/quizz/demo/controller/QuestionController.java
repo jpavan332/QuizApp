a5fc71ca06933495574938579f8b606a78399f6d
jpavan <jpavan332@gmail.com>
2023-11-23 16:15:15 -0600

    Created Service package and moved controller to controller package

--- QuizApp/src/main/java/com/quizz/demo/controller/QuestionController.java
+++ QuizApp/src/main/java/com/quizz/demo/controller/QuestionController.java
@@ -1,16 +1,21 @@
-package com.quizz.demo;
+package com.quizz.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quizz.demo.service.QuestionService;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
-
+	
+	@Autowired
+	QuestionService questionService;
 	@GetMapping("allQuestions")
 	public String getAllQuestions() {
-		return "These are your questions";
+		return questionService.getAllQuestions();
 	}
 	
 }
---
+++ QuizApp/src/main/java/com/quizz/demo/service/QuestionService.java
@@ -0,0 +1,13 @@
+package com.quizz.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+
+	public String getAllQuestions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

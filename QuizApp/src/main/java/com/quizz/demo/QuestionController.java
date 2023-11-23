ba93d616e7749b80c1cb5a68adfe9c39104f89d3
jpavan <jpavan332@gmail.com>
2023-11-23 16:01:37 -0600

    updated base package name

--- QuizApp/src/main/java/com/quizz/demo/QuestionController.java
+++ QuizApp/src/main/java/com/quizz/demo/QuestionController.java
@@ -1,4 +1,4 @@
-package com.telsuko.demo;
+package com.quizz.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- QuizApp/src/main/java/com/quizz/demo/QuizAppApplication.java
+++ QuizApp/src/main/java/com/quizz/demo/QuizAppApplication.java
@@ -1,4 +1,4 @@
-package com.telsuko.demo;
+package com.quizz.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

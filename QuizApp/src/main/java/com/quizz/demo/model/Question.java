ebe092e6dc4a0f12454a1b877c55d8d764aa7cd4
jpavan <jpavan332@gmail.com>
2023-11-27 09:44:36 -0600

    configured JPA repository access but printing empty data to UI so added getter methods instead of lombok

--- QuizApp/src/main/java/com/quizz/demo/model/Question.java
+++ QuizApp/src/main/java/com/quizz/demo/model/Question.java
@@ -8,7 +8,6 @@ import lombok.Data;
 
 
 
-@Data
 @Entity
 public class Question {
 
@@ -18,11 +17,39 @@ public class Question {
 		private String questionTitle;
 		private String category;
 		private String difficultyLevel;
-		private String rightAnswer;
 		private String option1;
 		private String option2;
 		private String option3;
 		private String option4;
+		private String rightAnswer;
+		public int getId() {
+			return id;
+		}
+		public String getQuestionTitle() {
+			return questionTitle;
+		}
+		public String getCategory() {
+			return category;
+		}
+		public String getDifficultyLevel() {
+			return difficultyLevel;
+		}
+		public String getOption1() {
+			return option1;
+		}
+		public String getOption2() {
+			return option2;
+		}
+		public String getOption3() {
+			return option3;
+		}
+		public String getOption4() {
+			return option4;
+		}
+		public String getRightAnswer() {
+			return rightAnswer;
+		}
+		
 		
 		
 }
--- QuizApp/src/main/java/com/quizz/demo/service/QuestionService.java
+++ QuizApp/src/main/java/com/quizz/demo/service/QuestionService.java
@@ -1,17 +1,28 @@
 package com.quizz.demo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.quizz.demo.dao.Questiondao;
 import com.quizz.demo.model.Question;
 
 @Service
 public class QuestionService {
+	
+	@Autowired
+	Questiondao questionDao;
 
 	public List<Question> getAllQuestions() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Question> q1 = new ArrayList<Question>();
+		q1 = questionDao.findAll();
+		for(Question q2: q1)
+			System.out.println(q2);
+		return questionDao.findAll();
+		
 	}
 
 	
